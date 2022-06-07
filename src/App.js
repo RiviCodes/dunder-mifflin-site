4553e1dbe1260251809a24e3585ab29e2f3fa02f
RiviCodes <jose.riveramz@udlap.mx>
2022-06-07 14:34:54 -0500

    Import Hero component, render component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }

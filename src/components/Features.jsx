49da6e4b4345f0e47f633e564cfc8d808a3b61a6
RiviCodes <jose.riveramz@udlap.mx>
2022-06-07 18:54:20 -0500

    Edit paragraphs size, edit icons size

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,55 +2,55 @@ import React from "react";
 
 export default function Features() {
   return (
-    <div className="containter mx-3 my-2">
+    <div className="containter mx-3 mt-4">
       <div className="row">
         {/* Card #1 */}
-        <div className="col-6 mb-5">
+        <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-bootstrap-fill display-3 text-indigo-400"></i>
-            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+            <i class="bi bi-bootstrap-fill display-4 text-indigo-400"></i>
+            <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
-            <p className="h4 fw-light h-auto w-75">
+            <p className="h5 fw-light h-auto w-75">
               Paragraph of text beneath the heading to explain the heading.
             </p>
           </div>
         </div>
 
         {/* Card #2 */}
-        <div className="col-6 mb-5">
+        <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-cpu-fill display-3 text-indigo-400"></i>
-            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+            <i class="bi bi-cpu-fill display-4 text-indigo-400"></i>
+            <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
-            <p className="h4 fw-light h-auto w-75">
+            <p className="h5 fw-light h-auto w-75">
               Paragraph of text beneath the heading to explain the heading.
             </p>
           </div>
         </div>
 
         {/* Card #3 */}
-        <div className="col-6 mb-5">
+        <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-calendar3 display-3 text-indigo-400"></i>
-            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+            <i class="bi bi-calendar3 display-4 text-indigo-400"></i>
+            <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
-            <p className="h4 fw-light h-auto w-75">
+            <p className="h5 fw-light h-auto w-75">
               Paragraph of text beneath the heading to explain the heading.
             </p>
           </div>
         </div>
 
         {/* Card #4 */}
-        <div className="col-6 mb-5">
+        <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-house-door display-3 text-indigo-400"></i>
-            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+            <i class="bi bi-house-door display-4 text-indigo-400"></i>
+            <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
-            <p className="h4 fw-light h-auto w-75">
+            <p className="h5 fw-light h-auto w-75">
               Paragraph of text beneath the heading to explain the heading.
             </p>
           </div>

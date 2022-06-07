e310b38872dc28f811de1c4556d269da58122a72
RiviCodes <jose.riveramz@udlap.mx>
2022-06-07 15:32:39 -0500

    Add test of Cards, add icons, add paragraphs

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,43 @@
 import React from "react";
 
 export default function Features() {
-  return <></>;
+  return (
+    <div className="containter mx-4">
+      <div className="row">
+        {/* Card */}
+        <div className="col-6 d-flex">
+          {/* Card's LEFT side */}
+          <div className="col d-none d-md-block">
+            <i class="bi bi-bootstrap-fill"></i>
+          </div>
+
+          {/* Card's RIGHT side */}
+          <div className="col flex-column">
+            <i class="bi bi-bootstrap-fill display-4 text-indigo-400"></i>
+            <p className="h2 fw-bolder h-auto my-3">Featured title</p>
+            <p className="h4 fw-light h-auto text-start">
+              Paragraph of text beneath the heading to explain the heading.
+            </p>
+          </div>
+        </div>
+
+        {/* Card */}
+        <div className="col-6 d-flex">
+          {/* Card's LEFT side */}
+          <div className="col d-none d-md-block">
+            <i class="bi bi-bootstrap-fill"></i>
+          </div>
+
+          {/* Card's RIGHT side */}
+          <div className="col flex-column">
+            <i class="bi bi-bootstrap-fill display-4 text-indigo-400"></i>
+            <p className="h2 fw-bolder h-auto my-3">Featured title</p>
+            <p className="h4 fw-light h-auto text-start">
+              Paragraph of text beneath the heading to explain the heading.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

fbb5da24ad924f2aaace0ed11626b1b5d01b1656
RiviCodes <jose.riveramz@udlap.mx>
2022-06-07 18:11:21 -0500

    Edit container's width

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Features() {
   return (
-    <div className="containter mx-2 my-2">
+    <div className="containter mx-3 my-2">
       <div className="row">
         {/* Card #1 */}
         <div className="col-6 mb-4">
@@ -34,6 +34,19 @@ export default function Features() {
         <div className="col-6 mb-4">
           <div className="col d-flex flex-column align-items-center">
             <i class="bi bi-calendar3 display-4 text-indigo-400"></i>
+            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+              Featured title
+            </p>
+            <p className="h4 fw-light h-auto w-75">
+              Paragraph of text beneath the heading to explain the heading.
+            </p>
+          </div>
+        </div>
+
+        {/* Card #4 */}
+        <div className="col-6 mb-4">
+          <div className="col d-flex flex-column align-items-center">
+            <i class="bi bi-house-door display-4 text-indigo-400"></i>
             <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
               Featured title
             </p>

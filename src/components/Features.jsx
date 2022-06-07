1fe088fb19e5288348a79c5ff8761a82c5a4916c
RiviCodes <jose.riveramz@udlap.mx>
2022-06-07 18:03:38 -0500

    Delete second column, fix flex elements, add icons

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,37 +2,29 @@ import React from "react";
 
 export default function Features() {
   return (
-    <div className="containter mx-4">
+    <div className="containter mx-2 my-2">
       <div className="row">
         {/* Card */}
-        <div className="col-6 d-flex">
-          {/* Card's LEFT side */}
-          <div className="col d-none d-md-block">
-            <i class="bi bi-bootstrap-fill"></i>
-          </div>
-
-          {/* Card's RIGHT side */}
-          <div className="col flex-column">
+        <div className="col-6">
+          <div className="col d-flex flex-column align-items-center">
             <i class="bi bi-bootstrap-fill display-4 text-indigo-400"></i>
-            <p className="h2 fw-bolder h-auto my-3">Featured title</p>
-            <p className="h4 fw-light h-auto text-start">
+            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+              Featured title
+            </p>
+            <p className="d-flex justify-content-center h4 fw-light h-auto">
               Paragraph of text beneath the heading to explain the heading.
             </p>
           </div>
         </div>
 
         {/* Card */}
-        <div className="col-6 d-flex">
-          {/* Card's LEFT side */}
-          <div className="col d-none d-md-block">
-            <i class="bi bi-bootstrap-fill"></i>
-          </div>
-
-          {/* Card's RIGHT side */}
-          <div className="col flex-column">
-            <i class="bi bi-bootstrap-fill display-4 text-indigo-400"></i>
-            <p className="h2 fw-bolder h-auto my-3">Featured title</p>
-            <p className="h4 fw-light h-auto text-start">
+        <div className="col-6">
+          <div className="col d-flex flex-column align-items-center">
+            <i class="bi bi-cpu-fill display-4 text-indigo-400"></i>
+            <p className="d-flex justify-content-center h2 fw-bolder h-auto my-3">
+              Featured title
+            </p>
+            <p className="h4 fw-light h-auto">
               Paragraph of text beneath the heading to explain the heading.
             </p>
           </div>

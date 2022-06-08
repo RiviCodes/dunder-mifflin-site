9f914940e86ef2d2dfd838ebf749363350ca5003
RiviCodes <jose.riveramz@udlap.mx>
2022-06-08 17:54:25 -0500

    Import Card component, render component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Features />
+      <Card />
     </div>
   );
 }

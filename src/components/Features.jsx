cddbf751610efadb34bc8e5d0f16ca0ea819ac91
RiviCodes <jose.riveramz@udlap.mx>
2022-06-08 16:23:49 -0500

    Replace icons

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -7,7 +7,7 @@ export default function Features() {
         {/* Card #1 */}
         <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-bootstrap-fill display-4 text-indigo-400"></i>
+            <i class="bi bi-box-seam display-4 text-indigo-400"></i>
             <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
@@ -20,7 +20,7 @@ export default function Features() {
         {/* Card #2 */}
         <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-cpu-fill display-4 text-indigo-400"></i>
+            <i class="bi bi-calendar-check-fill display-4 text-indigo-400"></i>
             <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
@@ -33,7 +33,7 @@ export default function Features() {
         {/* Card #3 */}
         <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-calendar3 display-4 text-indigo-400"></i>
+            <i class="bi bi-house-door display-4 text-indigo-400"></i>
             <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>
@@ -46,7 +46,7 @@ export default function Features() {
         {/* Card #4 */}
         <div className="col-6 col-md-3 mb-5">
           <div className="col d-flex flex-column align-items-center">
-            <i class="bi bi-house-door display-4 text-indigo-400"></i>
+            <i class="bi bi-geo-fill display-4 text-indigo-400"></i>
             <p className="d-flex justify-content-center h3 fw-bolder h-auto my-3">
               Featured title
             </p>

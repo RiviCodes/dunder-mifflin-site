54707cdaecbd40237b48d55fad81919fc97a63ed
RiviCodes <jose.riveramz@udlap.mx>
2022-06-09 18:10:36 -0500

    Delete fragment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <>
-      <nav class="navbar bg-light shadow-sm fixed-top">
-        <div class="container-fluid d-flex justify-content-center">
-          <span class="navbar-brand mb-0 h1 fs-3 text-indigo-500">
-            <i class="bi bi-building"></i> Dunder Mifflin{" "}
-            <i class="bi bi-box2-heart-fill"></i>
-          </span>
-        </div>
-      </nav>
-    </>
+    <nav class="navbar bg-light shadow-sm fixed-top">
+      <div class="container-fluid d-flex justify-content-center">
+        <span class="navbar-brand mb-0 h1 fs-3 text-indigo-500">
+          <i class="bi bi-building"></i> Dunder Mifflin{" "}
+          <i class="bi bi-box2-heart-fill"></i>
+        </span>
+      </div>
+    </nav>
   );
 }

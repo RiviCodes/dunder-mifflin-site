5e6dae8f7e88b0f5fb993728f746d2e5d67f6046
RiviCodes <jose.riveramz@udlap.mx>
2022-06-08 23:00:45 -0500

    Implement Grid test

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,51 @@
 import React from "react";
 
 export default function Grid() {
-  return <></>;
+  return (
+    <div className="container">
+      <div className="row">
+        {/* LEFT COLUMN */}
+        <div className="col-12 col-lg-9">
+          {/* NESTED TOP ROW */}
+          <div className="row">
+            <div className="col-12 col-lg-8 bg-primary">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Dignissimos ea tempora, maiores fugiat maxime aliquam vel
+              exercitationem aspernatur labore, voluptates magnam illum iusto
+              ex. Optio deleniti nisi ipsum doloremque voluptatibus.
+            </div>
+            <div className="col-12 col-lg-4 bg-success">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Dignissimos ea tempora, maiores fugiat maxime aliquam vel
+              exercitationem aspernatur labore, voluptates magnam illum iusto
+              ex. Optio deleniti nisi ipsum doloremque voluptatibus.
+            </div>
+          </div>
+          {/* NESTED BOTTOM ROW */}
+          <div className="row">
+            <div className="col-12 col-lg-4 bg-warning">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Dignissimos ea tempora, maiores fugiat maxime aliquam vel
+              exercitationem aspernatur labore, voluptates magnam illum iusto
+              ex. Optio deleniti nisi ipsum doloremque voluptatibus.
+            </div>
+            <div className="col-12 col-lg-8 bg-danger">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Dignissimos ea tempora, maiores fugiat maxime aliquam vel
+              exercitationem aspernatur labore, voluptates magnam illum iusto
+              ex. Optio deleniti nisi ipsum doloremque voluptatibus.
+            </div>
+          </div>
+        </div>
+
+        {/* RIGHT COLUMN */}
+        <div className="col-12 col-lg-3 bg-secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorem
+          velit tempora architecto amet qui hic suscipit necessitatibus placeat
+          ex laudantium sed, consequuntur molestias asperiores ipsam aliquam.
+          Praesentium, velit sint.
+        </div>
+      </div>
+    </div>
+  );
 }

7b187531455455c85eeeac34279aaf438f88c130
RiviCodes <jose.riveramz@udlap.mx>
2022-06-08 23:16:09 -0500

    Add preview content of card

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,17 +2,32 @@ import React from "react";
 
 export default function Grid() {
   return (
-    <div className="container">
+    <div className="container d-flex align-items-center justify-content-center vh-100">
       <div className="row">
         {/* LEFT COLUMN */}
         <div className="col-12 col-lg-9">
           {/* NESTED TOP ROW */}
           <div className="row">
             <div className="col-12 col-lg-8 bg-primary">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Dignissimos ea tempora, maiores fugiat maxime aliquam vel
-              exercitationem aspernatur labore, voluptates magnam illum iusto
-              ex. Optio deleniti nisi ipsum doloremque voluptatibus.
+              <div class="card">
+                {/* SINGLE CARD'S CONTENT */}
+                <div class="card-body">
+                  <div className="d-flex">
+                    <img
+                      src="https://bolavip.com/__export/1640142627460/sites/bolavip/img/2021/12/22/johnkrasinski_crop1640142301901.jpg_1159711837.jpg"
+                      alt=""
+                      className="img-fluid w-25"
+                    />
+                    <div>
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text h6">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="col-12 col-lg-4 bg-success">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.

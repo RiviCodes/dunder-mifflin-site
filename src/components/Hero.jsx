d717c2aecc04cf52e723c2db52459ab16b107956
RiviCodes <jose.riveramz@udlap.mx>
2022-06-09 20:07:39 -0500

    Fix container's margin and padding, add hr

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div id="home" className="container mt-5 pt-5 pb-3">
+    <div id="home" className="container">
       <div className="row">
         {/* LEFT COLUMN */}
-        <div className="col-12 col-md-6">
+        <div className="col-12 col-md-6 mt-5 pt-4">
           {/* Display image ONLY when breakpoint is UNDER MD */}
           <div className="col-12 col-md-6 d-md-none mb-5">
             <img
@@ -16,7 +16,7 @@ export default function Hero() {
           </div>
 
           {/* Primary paragraph */}
-          <div className="col mb-4">
+          <div className="col mt-5 mb-4">
             <p className="h1 text-start h-auto w-75">
               <strong>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -43,7 +43,7 @@ export default function Hero() {
         </div>
 
         {/* RIGHT COLUMN, display ONLY when breakpoint is OVER MD */}
-        <div className="col-md-6 d-none d-md-block">
+        <div className="col-md-6 d-none d-md-block mt-5 pt-4">
           <div className="col">
             <img
               src="https://wallpapers.com/images/high/the-office-dunder-mifflin-boss-87ucljukpbj1p3iw.jpg"
@@ -53,6 +53,7 @@ export default function Hero() {
           </div>
         </div>
       </div>
+      <hr />
     </div>
   );
 }
